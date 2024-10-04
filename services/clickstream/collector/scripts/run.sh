f4d7bc323add70b1e5bc9bca9868dceb6180ffb5
csgn <dev.csgn@gmail.com>
2024-10-04 13:44:22 +0300

    Remove scripts folder

--- services/clickstream/collector/scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-COLLECTOR_VERSION=$(cat version) \
-COLLECTOR_HOST=0.0.0.0 \
-COLLECTOR_PORT=9999 \
-KAFKA_HOST=0.0.0.0 \
-KAFKA_PORT=9092 \
-KAFKA_TOPIC=clickstream-test \
-go run ./cmd/collector
