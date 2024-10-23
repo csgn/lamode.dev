2e12f68bd4210aefc91eebfa07b06c76c784c02c
csgn <dev.csgn@gmail.com>
2024-10-23 23:16:19 +0300

    Add hadoop service

---
+++ services/clickstream/src/hadoop/scripts/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+sudo service ssh start
+
+if [ ! -d "/tmp/hadoop/dfs/name" ]; then
+    $HADOOP_HOME/bin/hdfs namenode -format && echo "OK : HDFS namenode format operation finished successfully !"
+fi
+
+$HADOOP_HOME/sbin/start-dfs.sh
+
+echo "YARNSTART = $YARNSTART"
+if [[ -z $YARNSTART || $YARNSTART -ne 0 ]]; then
+    echo "running start-yarn.sh"
+    $HADOOP_HOME/sbin/start-yarn.sh
+fi
+
+$HADOOP_HOME/bin/hdfs dfs -mkdir /data
+$HADOOP_HOME/bin/hdfs dfs -mkdir /tmp
+$HADOOP_HOME/bin/hdfs dfs -mkdir /users
+$HADOOP_HOME/bin/hdfs dfs -mkdir /checkpoints
+$HADOOP_HOME/bin/hdfs dfs -chmod 777 /data
+$HADOOP_HOME/bin/hdfs dfs -chmod 777 /tmp
+$HADOOP_HOME/bin/hdfs dfs -chmod 777 /users
+$HADOOP_HOME/bin/hdfs dfs -chmod 777 /checkpoints
+$HADOOP_HOME/bin/hdfs dfsadmin -safemode leave
+
+# keep the container running indefinitely
+tail -f $HADOOP_HOME/logs/hadoop-*-namenode-*.log
