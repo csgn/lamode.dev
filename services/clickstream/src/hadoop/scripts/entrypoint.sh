2ef44baf4078f52fd674acf16b2ede699824bec7
csgn <dev.csgn@gmail.com>
2024-11-06 22:16:21 +0300

    Refactor hdfs config

--- services/clickstream/src/hadoop/scripts/entrypoint.sh
+++ services/clickstream/src/hadoop/scripts/entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 sudo service ssh start
+# sudo service cron start
 
 if [ ! -d "/tmp/hadoop/dfs/name" ]; then
     $HADOOP_HOME/bin/hdfs namenode -format && echo "OK : HDFS namenode format operation finished successfully !"
@@ -16,6 +17,10 @@ if [[ -z $YARNSTART || $YARNSTART -ne 0 ]]; then
 fi
 
 $HADOOP_HOME/bin/hdfs dfs -mkdir /data
+$HADOOP_HOME/bin/hdfs dfs -mkdir /data/raw
+$HADOOP_HOME/bin/hdfs dfs -mkdir /data/stage
+$HADOOP_HOME/bin/hdfs dfs -mkdir /data/archive
+$HADOOP_HOME/bin/hdfs dfs -mkdir /data/final
 $HADOOP_HOME/bin/hdfs dfs -mkdir /tmp
 $HADOOP_HOME/bin/hdfs dfs -mkdir /users
 $HADOOP_HOME/bin/hdfs dfs -mkdir /checkpoints
@@ -24,6 +29,8 @@ $HADOOP_HOME/bin/hdfs dfs -chmod 777 /tmp
 $HADOOP_HOME/bin/hdfs dfs -chmod 777 /users
 $HADOOP_HOME/bin/hdfs dfs -chmod 777 /checkpoints
 $HADOOP_HOME/bin/hdfs dfsadmin -safemode leave
+
+# crontab -u root /app/cron 
 
 # keep the container running indefinitely
 tail -f $HADOOP_HOME/logs/hadoop-*-namenode-*.log
